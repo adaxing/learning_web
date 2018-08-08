76c1096592a32354334ae620a7993883576e865c
adaxing <xing2@ualberta.ca>
2018-08-08 11:29:48 -0600

    update

---
+++ intro_js/ex.js
@@ -0,0 +1,11 @@
+var body = document.querySelector("body"); 
+var isBlue = false;
+
+setInterval(function(){
+	if (isBlue) {
+		body.style.background = "white"; 
+	} else {
+		body.style.background = "red";
+	}
+	isBlue = lisBlue;
+}, 1000);
