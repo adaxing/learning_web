19c724d0d07dfac00a46121e05a322e4f9f5f966
adaxing <xing2@ualberta.ca>
2018-08-22 19:28:49 +0000

    backend changing stylesheet

--- MoreExpress/EJSdemo/app.js
+++ MoreExpress/EJSdemo/app.js
@@ -6,10 +6,23 @@ app.get("/", function(req,res){
     res.render("home.ejs");
 })
 
+// <%= %> --- return to html
+// <% %> --- just run as javascript, purely to run logically 
 app.get("/fallinlovewith/:thing", function(req,res){
     var thing = req.params.thing;
     res.render("love.ejs", {thingVar: thing});
 })
+
+app.get("/posts", function(req,res){
+    var posts = [
+        {title:"Post 1", author:"Susy"},
+        {title:"My adorable pet", author:"Charlie"},
+        {title:"Can you believe this pomsky?", author:"Colt"},
+    ];
+    // {a:b} = b is variable, a is inside ejs
+    res.render("posts.ejs", {posts: posts});
+})
+
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("Server has started!");
 })
