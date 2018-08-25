bad4830144c84c4171a9d5aba1f9d7882a13a887
adaxing <xing2@ualberta.ca>
2018-08-25 22:13:46 +0000

    done for project BLOG

--- RESTfulRouting/RESTfulBlogApp/app.js
+++ RESTfulRouting/RESTfulBlogApp/app.js
@@ -1,13 +1,18 @@
 var express = require("express"),
     app = express(),
     bodyParser = require("body-parser"),
-    mongoose = require("mongoose");
+    mongoose = require("mongoose"),
+    methodOverride = require("method-override"),
+    expressSanitizer = require("express-sanitizer");
 
 mongoose.connect("mongodb://localhost/restful_blog_app");
 app.set("view engine", "ejs");
 // serve customer stylesheet
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(expressSanitizer());
+// "_method" as parameter can be used for put, get, delete, update as .... request
+app.use(methodOverride("_method"));
 
 // schema
 // title, image, body, created
@@ -49,6 +54,7 @@ app.get("/blogs/new", function(req,res){
 });
 // create route
 app.post("/blogs", function(req,res){
+    req.body.blog.body = req.sanitize(req.body.blog.body);
     // create blog
     Blog.create(req.body.blog, function(err, newBlog){
         if(err){
@@ -69,6 +75,40 @@ app.get("/blogs/:id", function(req,res){
         }
     });
 });
+// edit route: /blogs/:id/edit GET
+app.get("/blogs/:id/edit", function(req, res){
+    Blog.findById(req.params.id, function(err, foundBlog){
+        if(err){
+            res.redirect("/blogs");
+        } else{
+            res.render("edit", {blog: foundBlog});
+        }
+    });
+});
+// update route: /blogs/:id  PUT
+app.put("/blogs/:id", function(req,res){
+    req.body.blog.body = req.sanitize(req.body.blog.body);
+    // req.body.blog can be seen as whole part: has title, image url, content, date
+    Blog.findByIdAndUpdate(req.params.id, req.body.blog, function(err, updateBlog){
+        if(err){
+            res.redirect("/blogs");
+        } else{
+            res.redirect("/blogs/"+ req.params.id);
+        }
+    })
+});
+// destroy route: /blogs/:id DELETE
+app.delete("/blogs/:id", function(req,res){
+    // callback function does not have any data come back since it has already deleted
+    Blog.findByIdAndRemove(req.params.id, function(err){
+        if (err){
+            res.redirect("/blogs");
+        } else{
+            res.redirect("/blogs");
+        }
+    });
+});
+
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("Server has started!!!");
 });
