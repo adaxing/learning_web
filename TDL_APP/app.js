afbdc0b25b931714800ea8965eef6b69f9beca03
adaxing <xing2@ualberta.ca>
2018-09-04 05:31:13 +0000

    setup new proj

---
+++ TDL_APP/app.js
@@ -0,0 +1,25 @@
+var express = require("express");
+var app = express();
+
+app.set("view engine", "ejs");
+
+app.get("/", function(req,res){
+    res.render("home");
+});
+
+// index
+app.get("/list", function(req,res){
+    res.render("list");
+});
+
+app.get("/register", function(req,res){
+    res.render("register");
+});
+
+app.get("/login", function(req,res){
+    res.render("login");
+});
+
+app.listen(process.env.PORT, process.env.IP, function(){
+    console.log("Server has started");
+});
